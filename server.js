56209b3f747f05c06a2a0fcd7acc45384e0b7659
karimm327 <benhajkarim73@gmail.com>
2025-11-21 23:30:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use(cors());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 // ----------------- Stripe -----------------
 const stripe = new Stripe(process.env.STRIPE_SECRET);
